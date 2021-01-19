dbdd1f678c4a344e676364fb0d31c68231b9f189
ErikdelRio <erik.delrio@ciencias.unam.mx>
2021-01-18 21:27:43 -0600

    Menú cifrar y descifrar

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
+from getpass import getpass
+
+def menuCifrar():
+    archivo = input ('Archivo a cifrar: \t')
+    if not archivoExiste(archivo, 'No se encontró el archivo a cifrar'):
+        return
+    archivoCifrado = input ('Archivo de evaluaciones: \t')
+    evaluaciones = leerNatural ('Número de evaluaciones requeridas')
+    necesarios = leerNatural ('Número de puntos necesarios')
+    contrasenia = getpass("Contraseña: ")
+
+    cifrar (archivo, archivoCifrado, evaluaciones, necesarios, contrasenia)
+
+def menuDescifrar():
+    archivoCifrado = input ('Archivo a descifrar: \t')
+    if not archivoExiste(archivoCifrado, "No se encontró el archivo a descifrar"):
+        return
+
+    archivoEvaluaciones = input ('Archivo con evaluaciones: \t')
+    if not archivoExiste(archivoEvaluaciones, "No se econtró el archivo con las evaluaciones"):
+        return
+
+    descifrar (archivoCifrado, archivoEvaluaciones)
+
+def leerNatural(mensaje):
+    natural = 0
+    correcto = false
+    while not correcto:
+        try:
+            natural = int(input(mensaje))
+            correcto = true
+        except ValueError:
+            print ('Introduce un natural')
+    return natural
+
+def archivoExiste(arch, mensajeError):
+    if not os.path.isfile (arch):
+        print (mensajeError)
+        return false
+    return true
 
 salir = False
 opcion = ''
@@ -13,9 +53,11 @@ while not salir:
     opcion = input().upper()
 
     if opcion == 'C':
-        print ('vamos a cifrar')
+        print ('vamos a cifrar\n')
+        menuCifrar()
     elif opcion == 'D':
         print ('vamos a descifrar')
+        menuDescifrar()
     elif opcion == 'S':
         salir = True
     else:
